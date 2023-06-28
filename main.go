3da100cb555806718d3fab4261b33ab7c5cdbed0
Green <green@greenthepear.com>
2023-06-28 23:24:19 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+type Card struct {
+	name    string
+	cost    int
+	damage  int
+	block   int
+	effects Effect
+}
+
+type Effect struct {
+	weaken     int
+	vulnerable int
+}
+
+type Player struct {
+	hp          int
+	hpMax       int
+	energy      int
+	energyMax   int
+	effects     Effect
+	deck        []Card
+	drawPile    []Card
+	discardPile []Card
+	hand        []Card
+}
+
+type Enemy struct {
+	name    string
+	hp      int
+	hpMax   int
+	intent  int
+	damage  int
+	effects Effect
+}
+
+func (p *Player) draw(numOfCards int) {
+
+}
+
+func combatStart(player *Player, enemy *Enemy) {
+	player.drawPile = player.deck
+}
+
+func createCardSliceByReferanceIntSlice(cardTypes []Card, cardIntSlice []int) []Card {
+	rSlice := make([]Card, len(cardIntSlice))
+	for i, cardNumber := range cardIntSlice {
+		rSlice[i] = cardTypes[cardNumber]
+	}
+	return rSlice
+}
+
+func main() {
+	cards := []Card{
+		{"Strike", 1, 7, 0, Effect{0, 0}},
+		{"Duck", 1, 0, 5, Effect{0, 0}},
+		{"Shock", 0, 0, 3, Effect{2, 0}},
+		{"Crush", 2, 3, 3, Effect{1, 1}},
+	}
+
+	startingDeck := []int{0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 3}
+
+	player := Player{
+		hp:        0, //start at 0 to test combatStart
+		hpMax:     50,
+		energy:    0,
+		energyMax: 3,
+		effects:   Effect{0, 0},
+		deck:      createCardSliceByReferanceIntSlice(cards, startingDeck),
+	}
+
+	enemy := Enemy{
+		name:    "Duende",
+		hp:      20,
+		hpMax:   20,
+		intent:  0,
+		damage:  5,
+		effects: Effect{0, 0},
+	}
+	combatStart(&player, &enemy)
+
+}
